40a676e06a90dadd9a3c41c5836a4a93631d8712
Suvayu Ali <Suvayu.Ali@cern.ch>
2011-12-13 16:13:29 +0100

    Update script

--- readTree/compile.bash
+++ readTree/compile.bash
@@ -4,12 +4,16 @@
 # NOTE: compiling in same session links the 2nd library with the 1st.
 # Make sure to avoid that.
 
-root -l -b <<EOF
+if [[ $1 = MC ]]; then
+    root -l -b <<EOF
 .L readMCTree.cxx++
 .q
 EOF
-
-root -l -b <<EOF
+elif [[ $1 = Data ]]; then
+    root -l -b <<EOF
 .L readDataTree.cxx++
 .q
 EOF
+fi
+
+exit $?
