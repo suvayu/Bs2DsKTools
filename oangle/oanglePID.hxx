af374c3b21af7dcca987caeadc6a62596b853672
Suvayu Ali <Suvayu.Ali@cern.ch>
2011-12-19 17:18:30 +0100

    Reimplement PID with kinematic variables
    Implemented as the header only class, oanglePID. Regularise the
    reference histogram bin content before calculating the DLL.

--- oangle/oanglePID.hxx
+++ oangle/oanglePID.hxx
@@ -2,7 +2,9 @@
 #define __OANGLEPID_HXX
 
 #include <string>
+#include <limits>
 
+#include <TMath.h>
 #include <TH2D.h>
 
 using namespace std;
@@ -17,24 +19,64 @@ public:
    * @param dsk Pointer to DsK histogram
    * @param dspi Pointer to Dspi histogram
    */
-  oanglePID(double wt, TH2D *dsk, TH2D *dspi) : weight(wt), hDsK(dsk), hDspi(dspi) {}
-  oanglePID(double wt, string sDsK="hDsK", string sDspi="hDspi", string fname="histos.root");
+  oanglePID(TH2D* dsk, TH2D* dspi) : hDsK(dsk), hDspi(dspi) {}
+
+  // // FIXME: Do not use, doesn't work. Histograms go out of scope and segfaults later
+  // oanglePID(string sDsK="hDsK", string sDspi="hDspi", string fname="histos.root") {}
+
   ~oanglePID() {}
 
   /**
    * Return PID delta log likelihood based on kinematics variables for
-   * DsK and Dspi events
+   * DsK and Dspi events.
    *
    * @param Bsmass Bs mass
    * @param oangleCosine Cosine of the opening angle
    *
-   * @return delta log likelihood
+   * @return delta log likelihood (DLL = ln[pDsK/pDspi])
    */
-  double GetoangleDLL(double Bsmass, double oangleCosine);
+  double GetoangleDLL(double Bsmass, double oangleCosine) {
+    // same binning for DsK and Dspi
+    return GetoangleDLL( hDsK->FindBin( Bsmass, oangleCosine) );
+  }
+
+  /** 
+   * Return PID delta log likelihood based on kinematics variables for
+   * DsK and Dspi events.
+   *
+   * @param gBin Global histogram bin number
+   *
+   * @return delta log likelihood (DLL = ln[pDsK/pDspi])
+   */
+  double GetoangleDLL(int gBin) { // same binning for DsK and Dspi
+    // normalise the bin content before getting the DLL
+    double pDsK ((hDsK ->GetBinContent(gBin) + numeric_limits<double>::epsilon()));
+    double pDspi((hDspi->GetBinContent(gBin) + numeric_limits<double>::epsilon()));
+    return DLL( pDsK, pDspi );
+  }
+
+  /** 
+   * Return delta log likelihood for given numerator and denominator.
+   *
+   * @param num numerator
+   * @param denom denominator
+   *
+   * @return delta log likelihood (DLL = ln[pDsK/pDspi])
+   */
+  static double DLL(double num, double denom) { return TMath::Log(num/denom); }
+
+  /** 
+   * Print reference histograms.
+   *
+   * @param opt Print options for TH2::Print()
+   */
+  void   PrintHistos(const char* opt="") {
+    hDsK ->Print(opt);
+    hDspi->Print(opt);
+  }
 
 private:
 
-  double weight;                /**< Relative weight between DsK and Dspi probabilities */
   TH2D  *hDsK;                  /**< Normalised 2-D DsK histogram */
   TH2D  *hDspi;                 /**< Normalised 2-D Dspi histogram */
 
