ccb2d04c2e366203b34b1b7dc2aba4153db35a17
Suvayu Ali <Suvayu.Ali@cern.ch>
2011-12-20 19:56:16 +0100

    Write test for kinematic PID variable

--- oangle/PIDsel.cc
+++ oangle/PIDsel.cc
@@ -3,6 +3,7 @@
 #include <TStyle.h>
 #include <TFile.h>
 #include <TH2D.h>
+#include <TCanvas.h>
 #include <TPad.h>
 
 #include "oanglePID.hxx"
@@ -12,6 +13,8 @@
 // #ifdef __CINT__
 // gSystem->Load("oanglePID_cxx.so");
 // #endif
+
+// Dsπ / DsK = 0.067 or 0.081
 
 using namespace std;
 
@@ -66,12 +69,18 @@ int PIDsel()
   hdataDsK.Draw("COLZ");
   gPad->Print("Bs-opening-angle-data-new-PID-K.png");
 
+  hdataBsmK.Print("base");
+  hdataDsK .Print("base");
+
   return 0;
 }
 
 
 int test_oanglePID()
 {
+  gStyle->SetPalette(1); // "rainbow" color palette
+  gStyle->SetNumberContours(256); // smooth color palette
+
   TFile *fhisto = new TFile( "templates.root", "read");
 
   TH2D *hDsK  = dynamic_cast<TH2D*> (fhisto->Get("hDsK" )->Clone());
@@ -83,17 +92,31 @@ int test_oanglePID()
   hweight->Reset("ICESM");
 
   // TH2D binning - 150, 4500, 6000, 25, -1, 1);
-  double mass(4505.), cosine(-0.98);
   int gbin(0);
-  while (mass < 6000 && cosine < 1) {
-    gbin = hweight->FindBin(mass, cosine);
-    hweight->SetBinContent(gbin, test.GetoangleDLL(mass, cosine));
-    // cout << "bin: " << gbin;
-    // cout << ", DLL: " << test.GetoangleDLL(mass, cosine) << endl;
-    mass += 10; cosine += 0.08;
+  for (double mass(4505.); mass < 6000; mass+=10) {
+    for (double cosine(-0.98); cosine < 1; cosine+=0.08) {
+      gbin = hweight->FindBin(mass, cosine);
+      hweight->SetBinContent(gbin, test.GetoangleDLL(mass, cosine));
+      // cout << "bin: " << gbin;
+      // cout << ", DLL: " << test.GetoangleDLL(mass, cosine) << endl;
+    }
   }
 
+  TCanvas c("c", "template compare", 1500, 400);
+  c.Divide(3,1);
+
+  c.cd(1);
+  hDsK->Draw("COLZ");
+
+  c.cd(2);
+  hDspi->Draw("COLZ");
+
+  c.cd(3);
+  hweight->Draw("COLZ");
+
+  c.Print("new-PID-test.png");
   // hweight->Divide(hDsK, hDspi, 0.1, 0.9);
-  hweight->Draw("surf");
+  // hweight->Draw("surf");
+
   return 0;
 }
