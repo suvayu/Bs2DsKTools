bfd33b8cc2d7c674d39462b0f2fd5d3cbda55a5c
Suvayu Ali <Suvayu.Ali@cern.ch>
2012-02-22 14:57:19 +0100

    Add some parsing and histogram utilities

--- utils/utils.cc
+++ utils/utils.cc
@@ -1,32 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 #include "utils.hh"
-
-using namespace std;
-
-bool Parsers::PrintOpts(TString& opt, TString& format)
-{
-  opt.ToLower();
-  bool doPrint(false);
-
-  if ( opt.Contains("print") ) {
-    doPrint = true;
-    if ( opt.Contains("png") )          format = "png";
-    else if ( opt.Contains("jpg") )     format = "jpg";
-    else if ( opt.Contains("ps") )      format = "ps";
-    else if ( opt.Contains("pdf") )     format = "pdf";
-    else if ( opt.Contains("cscript") ) format = "C";
-    else {
-      cout << "Error   Parsers::PrintOpts(): Bad print option!"
-	   << " No known formats found.\n"
-	   << "Warning Parsers::PrintOpts(): Printing will be skipped."
-	   << endl;
-      doPrint = false;
-    }
-  }
-
-  return doPrint;
-}
 
 
 TStyle* Style::setStyle()
@@ -37,4 +13,66 @@ TStyle* Style::setStyle()
   gStyle->SetNumberContours(256); // smooth color palette
   gStyle->SetTitleOffset( 1.2, "xy");
   return gStyle;
+}
+
+
+void Hist::rescale(TH1 *h, Double_t factor)
+{
+  // change X scale
+  Double_t xmin = h->GetXaxis()->GetXmin();
+  Double_t xmax = h->GetXaxis()->GetXmax();
+  h->GetXaxis()->SetLimits(xmin*factor,xmax*factor);
+}
+
+
+bool Parsers::PrintOpts(TString& opt, TString& format)
+{
+  opt.ToLower();
+  bool doPrint(false);
+  if ( opt.Contains("print") ) {
+    doPrint = true;
+    if ( opt.Contains("png") )          format = "png";
+    else if ( opt.Contains("jpg") )     format = "jpg";
+    else if ( opt.Contains("ps") )      format = "ps";
+    else if ( opt.Contains("pdf") )     format = "pdf";
+    else if ( opt.Contains("cscript") ) format = "C";
+    else {
+      std::cout << "Error   Parsers::PrintOpts(): Bad print option!"
+		<< " No known formats found.\n"
+		<< "Warning Parsers::PrintOpts(): Printing will be skipped."
+		<< std::endl;
+      doPrint = false;
+    }
+  }
+  return doPrint;
+}
+
+
+void Parsers::readconf(std::vector<TString> &var, std::vector<TString> &val, std::string fname)
+{
+  ifstream inFile(fname.c_str());
+
+  while (! inFile.eof()) {
+    TString tmp;
+    tmp.ReadToken(inFile);
+    if (tmp.BeginsWith("#")) {
+      tmp.ReadLine(inFile);
+      continue;
+    }
+    var.push_back(tmp);
+    tmp.ReadToken(inFile);
+    val.push_back(tmp);
+  }
+}
+
+
+std::string& Parsers::replaceAll(std::string& context, const std::string& from, const std::string& to)
+{
+  size_t lookHere = 0;
+  size_t foundHere;
+  while((foundHere = context.find(from, lookHere)) != string::npos) {
+    context.replace(foundHere, from.size(), to);
+    lookHere = foundHere + to.size();
+  }
+  return context;
 }
