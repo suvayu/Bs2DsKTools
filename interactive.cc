edb64767aff48f75c9668b846dc238bb70a90da9
Suvayu Ali <Suvayu.Ali@cern.ch>
2012-02-28 19:04:55 +0100

    Update interactive setup script (breaking change)

---
+++ interactive.cc
@@ -0,0 +1,7 @@
+{
+  gSystem->Load("libreadTree.so");
+  gSystem->Load("libutils.so");
+  gSystem->Load("libaccept.so");
+  gSystem->Load("liboangle.so");
+  gSystem->Load("libPIDsel.so");
+}
--- src/interactive.cc
+++
@@ -1,5 +0,0 @@
-{
-  gSystem->Load("lifetime_cxx.so");
-  gSystem->Load("../utils/utils_cc.so");
-  gSystem->Load("accept_cc.so");
-}
