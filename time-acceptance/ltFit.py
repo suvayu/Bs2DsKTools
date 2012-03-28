7c3c04cfc8563f6d6f53bfee27532117c3e1cd6d
Suvayu Ali <Suvayu.Ali@cern.ch>
2012-03-28 16:53:19 +0200

    Gaussian resolution model for lifetime

--- time-acceptance/ltFit.py
+++ time-acceptance/ltFit.py
@@ -20,6 +20,7 @@ ftree           = ffile.Get('ftree')
 # parameters and observables
 time            = RooRealVar('time'    , 'lifetime in ns', 0.0001, 0.01)
 turnon          = RooRealVar('turnon'  , 'turnon'        , 500, 5000)
+exponent        = RooRealVar('exponent', 'exponent'      , 2, 4)
 
 # trigger
 trigger         = 'HLT2Topo3BodyTOS'
@@ -38,16 +39,30 @@ wdataset        = RooDataSet('wdataset', 'Weighted dataset',
                              RooFit.WeightVar(wt), RooFit.Import(ftree),
                              RooFit.Cut(cut))
 
-# decay
-decayH          = RooExponential('decayH', 'Decay function for the B_{s,H}',
-                                 time, RooRealConstant.value(-1E3/1.536875))
-decayL          = RooExponential('decayL', 'Decay function for the B_{s,L}',
-                                 time, RooRealConstant.value(-1E3/1.407125))
-decay           = RooAddPdf('decay', 'Decay function for the B_{s}',
-                            decayH, decayL, RooRealConstant.value(0.5))
+# # exponential decay
+# decayH          = RooExponential('decayH', 'Decay function for the B_{s,H}',
+#                                  time, RooRealConstant.value(-1E3/1.536875))
+# decayL          = RooExponential('decayL', 'Decay function for the B_{s,L}',
+#                                  time, RooRealConstant.value(-1E3/1.407125))
+# decay           = RooAddPdf('decay', 'Decay function for the B_{s}',
+#                             decayH, decayL, RooRealConstant.value(0.5))
+
+# resolution model
+resmodel        = RooGaussModel('resmodel', 'Time resolution model', time,
+                                RooRealConstant.value(0), RooRealConstant.value(4E-5))
+# decay model
+decayH          = RooDecay('decayH', 'Decay function for the B_{s,H}', time,
+                           RooRealConstant.value(1.536875/1E3), resmodel,
+                           RooDecay.SingleSided)
+decayL          = RooDecay('decayL', 'Decay function for the B_{s,L}', time,
+                           RooRealConstant.value(1.407125/1E3), resmodel,
+                           RooDecay.SingleSided)
+decay           = RooAddPdf('decay', 'Decay function for the B_{s}', decayH, decayL,
+                            RooRealConstant.value(0.5))
 
 # acceptance
-acceptance    = RooFormulaVar('acceptance', '1-1/(1+(@0*@1)**3)', RooArgList(turnon, time))
+acceptance      = RooFormulaVar('acceptance', '1-1/(1+(@0*@1)**@2)',
+                                RooArgList(turnon, time, exponent))
 acceptancePdf   = RooGenericPdf('acceptancePdf', '@0',
                                 RooArgList(acceptance))
 
@@ -57,13 +72,11 @@ Model.fitTo(dataset, RooFit.Range(0.0001, 0.01))
 
 tframe1         = time.frame(RooFit.Name('pfit'), RooFit.Title('Lifetime acceptance with Monte Carlo'))
 dataset.plotOn(tframe1, RooFit.MarkerStyle(ROOT.kFullTriangleUp))
-Model  .plotOn(tframe1)
+Model  .plotOn(tframe1, RooFit.LineColor(kBlue))
 
 tframe2         = time.frame(RooFit.Name('pmodel'), RooFit.Title('a(t) = decay(t) #times acc(t)'))
 wdataset     .plotOn(tframe2, RooFit.MarkerStyle(ROOT.kFullTriangleUp))
 decay        .plotOn(tframe2, RooFit.LineColor(ROOT.kRed))
-# decay      .plotOn(tframe2, LineColor(kRed-9), LineStyle(2), Components(decayL))
-# decay      .plotOn(tframe2, LineColor(kRed-1), LineStyle(2), Components(decayH))
 Model        .plotOn(tframe2, RooFit.LineColor(ROOT.kAzure))
 acceptancePdf.plotOn(tframe2, RooFit.LineColor(ROOT.kGreen))
 
@@ -74,4 +87,3 @@ tframe1.Draw()
 canvas .cd(2)
 tframe2.Draw()
 canvas.Print(trigger+'_ltFit_py.png')
-# canvas.Print('.png')
