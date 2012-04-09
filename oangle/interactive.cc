6248983413c30d4a5370054b77cf2478d30f1122
Suvayu Ali <Suvayu.Ali@cern.ch>
2012-04-09 22:12:32 +0200

    Cleanup legacy directories

--- oangle/interactive.cc
+++
@@ -1,5 +0,0 @@
-{
-  gSystem->Load("../utils/utils_cc.so");
-  gSystem->Load("oangle_cc.so");
-  gSystem->Load("PIDsel_cc.so");
-}
--- utils/compile.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-root -l -b <<EOF
-.L utils.cc++
-.q
-EOF
