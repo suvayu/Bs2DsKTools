f3281090cd4f80363b1a3383fc4d2b0da18a0501
Suvayu Ali <Suvayu.Ali@cern.ch>
2013-02-19 18:32:23 +0100

    Minor clean up of plotting script

--- python/plot-AcceptanceRatio.py
+++ python/plot-AcceptanceRatio.py
@@ -11,19 +11,13 @@ import sys
 # option parsing
 import argparse
 optparser = argparse.ArgumentParser(description=__doc__)
-optparser.add_argument('file1', help='ROOT file with fit result')
+optparser.add_argument('file', help='ROOT file with fit result')
 optparser.add_argument('-p', '--print', dest='doPrint', action='store_true',
                        help='Print plots to png/pdf files')
-
 options = optparser.parse_args()
 doPrint = options.doPrint
+fname = options.file
 
-fname1 = options.file1
-
-fname1tokens = fname1.split('-')
-accfntype1 = fname1tokens[2]
-mode1 = fname1tokens[1]
-constoffset1 = fname1tokens[-1]
 
 # FIXME: Batch running fails on importing anything but gROOT
 # ROOT global variables
@@ -51,44 +45,33 @@ from factory import get_workspace, get_file, get_object, load_library
 load_library('libacceptance.so')
 from ROOT import PowLawAcceptance, AcceptanceRatio
 
-# Files with fitresults:
-# data/fitresult-DsK-ratio-*
 
 # Get objects from workspace
-workspace, ffile = get_workspace(fname1, 'workspace')
+workspace, ffile = get_workspace(fname, 'workspace')
 workspace.Print('v')
 
-# variables
+# Variables
 time = workspace.var('time')
 
 # DsK acceptance function
-ratio = workspace.function('ratio')
-# acceptance_fn = workspace.function('acceptance_fn')
-# DsK_acceptance = workspace.function('acceptance')
-# DsK_acceptance.SetName('%s_%s' % ('DsK', DsK_acceptance.GetName()))
-
 rturnon = workspace.var('rturnon')
 roffset = workspace.var('roffset')
 rbeta = workspace.var('rbeta')
+ratio = workspace.function('ratio')
+fitresult = workspace.obj('fitresult_PDF_dataset')
 
-fitresult = workspace.obj('fitresult_Model_dataset')
-
-# ratio_hist = ratio.createHistogram('ratio_hist', time)
-# ratio_hist.Draw()
-
+## Plot
 tframe = time.frame(RooFit.Title('Time acceptance ratio'))
-
 paramset = RooArgSet(rturnon, roffset, rbeta)
 ratio.plotOn(tframe, RooFit.VisualizeError(fitresult, paramset, 1, False))
 ratio.plotOn(tframe)
 
 ## Draw
-# RooFit
 tframe.Draw()
 
 if doPrint:
     gPad.Print('plots/DsK_ratio.png')
-    # gPad.Print('plots/DsK_ratio.pdf')
+    gPad.Print('plots/DsK_ratio.pdf')
 
 # NB: Do not close file, otherwise plot disappears
 # ffile.Close()
