88f6ae9d4378e39a1b376049e7829c227f0eb4ce
Suvayu Ali <Suvayu.Ali@cern.ch>
2013-05-02 13:01:11 +0200

    Add raw ratio plot to investigate 6.5 ps kink
    Conclusion: statistical fluctuation

--- python/raw-ratios.py
+++ python/raw-ratios.py
@@ -35,7 +35,7 @@ from ROOT import kGreen, kRed, kBlack, kBlue, kAzure, kYellow, kCyan
 from ROOT import kPlus, kFullDotMedium, kFullDotSmall
 
 # ROOT classes
-from ROOT import TCanvas
+from ROOT import TCanvas, TLegend
 
 # rootpy modules
 from rootpy.io import File
@@ -70,7 +70,8 @@ cuts.update(dict(trig = cuts['HLT1'] & cuts['HLT2']))
 htypes = [
     'nocuts', 'pid', 'bdt', 'trig',    # base
     'pid_bdt', 'pid_trig', 'bdt_trig', # permutations of 2
-    'pid_bdt_trig'                     # all
+    'pid_bdt_trig',                    # all
+    'pid_bdt_trig_zoom'
 ]
 
 # # for variable binning (quite horrible at the moment)
@@ -93,6 +94,10 @@ for htype in htypes:
                 cut = cut & cuts['bdt']
             elif token.find('trig') >= 0:
                 cut = cut & cuts['trig']
+            elif token.find('zoom') >= 0:
+                # overwrite histogram with new binning
+                hist = Hist(60, 5.0, 8.0, name='h' + modes[mode] + '_' + htype, type='D')
+                # handle warning, modify to say it is intended
             else:
                 print 'Unknown permutation of cuts, weird things will happen.'
         histograms[mode].append(trees[mode].Draw('time', cut, '', hist))
@@ -112,13 +117,34 @@ for zoom in range(2):
         ratio_histograms[idx].SetMarkerStyle(kPlus)
         ratio_histograms[idx].Divide(histograms[0][idx], histograms[1][idx])
         if zoom:
+            if htitle.find('zoom') >= 0: continue
             ratio_histograms[idx].GetXaxis().SetRangeUser(0.2, 1)
             ratio_histograms[idx].GetYaxis().SetRangeUser(0.0, 2)
         if idx == 0 or idx > 6:
             canvas0.cd()
         if idx > 0 and idx < 7:
             canvas.cd(idx)
-        ratio_histograms[idx].Draw('e1')
+        # anomally, printing Dsπ and DsK distributtions overlayed by scaled ratio
+        if htitle.find('zoom') >= 0:
+            histograms[0][idx].SetLineColor(kRed)
+            histograms[1][idx].SetLineColor(kAzure)
+            histograms[0][idx].Draw('hist')
+            histograms[1][idx].Draw('hist same')
+            ratio_histograms[idx].SetFillColor(kBlack)
+            ratio_histograms[idx].Scale(1000.0/ratio_histograms[idx].Integral())
+            for nbin in range(1, ratio_histograms[idx].GetNbinsX() + 1):
+                ratio_histograms[idx].SetBinContent(nbin, ratio_histograms[idx].GetBinContent(nbin) + 80.0)
+            ratio_histograms[idx].Draw('e3 same')
+            legend = TLegend(0.75, 0.3, 0.85, 0.5)
+            legend.SetLineColor(0)
+            legend.SetFillStyle(0)
+            legend.AddEntry(histograms[0][idx], 'DsK', 'l')
+            legend.AddEntry(histograms[1][idx], 'Ds#pi', 'l')
+            legend.AddEntry(ratio_histograms[idx], 'ratio', 'l')
+            legend.Draw()
+            gPad.Update()
+        else:
+            ratio_histograms[idx].Draw('e1')
         if doPrint and (idx == 0 or idx > 6):
             canvas0.Print('plots/DsK_ratio_'+str(idx)+'_'+str(zoom)+'.png')
             canvas0.Print('plots/DsK_ratio_'+str(idx)+'_'+str(zoom)+'.pdf')
