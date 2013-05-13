38810471b5f28cbc6da150c709d6eb34a6eaf12b
Suvayu Ali <Suvayu.Ali@cern.ch>
2013-05-13 21:55:51 +0200

    Add string sanitiser for source code

--- python/helpers.py
+++ python/helpers.py
@@ -4,6 +4,8 @@
 
 """
 
+import re
+
 def sanitise_str(string, splchars=True):
     "Sanitise string for use as labels or filenames (as per flag)."
 
@@ -13,7 +15,7 @@ def sanitise_str(string, splchars=True):
         # LHCb tuple prefixes
         string = string.replace('CHI2', ' #chi^{2}')
     else:
-        pass
+        string = sanitise_str_src(string)
 
     # LHCb tuple prefixes
     string = string.replace('lab0', 'B')
@@ -21,3 +23,9 @@ def sanitise_str(string, splchars=True):
     string = string.replace('TAU', 'decay time')
     string = string.replace('ERR', ' error')
     return string
+
+
+def sanitise_str_src(string):
+    "Sanitise strings for use in names in source code."
+
+    return re.sub('[-%.+*?=()\[\]{} ^]', '_', string)
