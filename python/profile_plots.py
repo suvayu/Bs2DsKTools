366ba16c8e6daf602583da7da11bab6082a626ad
Suvayu Ali <Suvayu.Ali@cern.ch>
2013-05-18 01:32:48 +0200

    Clean up.  Modes and variables structured as dicts

--- python/profile_plots.py
+++ python/profile_plots.py
@@ -31,17 +31,38 @@ from rootpy.plotting import Hist, Hist2D, Profile #, Legend
 
 
 # setup
-modes = { 'dsk' : 'DsK', 'dspi' : 'Ds#pi'}
-trees = {
-    'dsk'  : File('data/smalltree-really-new-MC-pre-PID-DsK.root').Get('ftree'),
-    'dspi' : File('data/smalltree-really-new-MC-pre-PID-DsPi.root').Get('ftree')
+modes = {
+    'dsk' : {
+        'name' : 'DsK',
+        'tree' : File('data/smalltree-really-new-MC-pre-PID-DsK.root').Get('ftree'),
+        'cuts' : 10,
+        'cols' : QROOT.kBlue
+    },
+    'dspi' : {
+        'name' : 'Ds#pi',
+        'tree' : File('data/smalltree-really-new-MC-pre-PID-DsPi.root').Get('ftree'),
+        'cuts' : 0,
+        'cols' : QROOT.kRed
+    }
 }
-variables = { 'pt' : 'hMom.Pt()', 'ip' : 'hIPchi2' } #, 'pid' : 'PIDK' }
-varbinning = { 'pt' : (100, 0, 4E4), 'ip' : (100, 0, 8E3) }
-prettyvars = { 'pt' : 'p_{T} (MeV/c)', 'ip' : 'IP #chi^{2}' }
-pidcuts = { 'dsk' : 10, 'dspi' : 0 }
-colours = { 'dsk' : QROOT.kBlue, 'dspi' : QROOT.kRed }
 
+variables = {
+    'pt' : {
+        'expr' : 'hMom.Pt()',
+        'binn' : (100, 0, 4E4),
+        'name' : 'p_{T} (MeV/c)',
+    },
+    'ip' : {
+        'expr' : 'hIPchi2',
+        'binn' : (100, 0, 8E3),
+        'name' : 'IP #chi^{2}',
+    },
+    # 'pid' : {
+    #     'expr' : 'PIDK',
+    #     'binn' : (100, -50, 50),
+    #     'name' : 'PIDK',
+    # }
+}
 
 # profile plots
 hprofiles = {}
@@ -49,14 +70,16 @@ for var in variables:
     hprofiles[var] = {}
     for mode in modes:
         hname='hprof_%s_%s' % (var, mode)
-        htitle='Profile plot of PIDK vs {0};{0};PIDK'.format(prettyvars[var])
-        hprofiles[var][mode] = Profile(*varbinning[var], name=hname, title=htitle,
+        htitle='Profile plot of PIDK vs {0};{0};PIDK'.format(variables[var]['name'])
+        hprofiles[var][mode] = Profile(*variables[var]['binn'], name = hname,
+                                       title = htitle,
                                        markerstyle = QROOT.kPlus,
-                                       markercolor = colours[mode],
-                                       linecolor = colours[mode])
+                                       markercolor = modes[mode]['cols'],
+                                       linecolor = modes[mode]['cols'])
         hprofiles[var][mode].SetMaximum(50)
         hprofiles[var][mode].SetMinimum(-50)
-        trees[mode].Draw('%s:PIDK' % variables[var], 'time<1', 'prof', hprofiles[var][mode])
+        modes[mode]['tree'].Draw('%s:PIDK' % variables[var]['expr'], 'time<1',
+                                 'prof', hprofiles[var][mode])
 
 
 # legend
@@ -71,10 +94,10 @@ axes = {}
 for var in variables:
     axes[var] = {}
     for mode in modes:
-        axes[var][mode] = QROOT.TGaxis(0, pidcuts[mode],
-                                       varbinning[var][2], pidcuts[mode],
-                                       0, varbinning[var][2], 0)
-        axes[var][mode].SetLineColor(colours[mode])
+        axes[var][mode] = QROOT.TGaxis(variables[var]['binn'][1], modes[mode]['cuts'],
+                                       variables[var]['binn'][2], modes[mode]['cuts'],
+                                       variables[var]['binn'][1], variables[var]['binn'][2], 0)
+        axes[var][mode].SetLineColor(modes[mode]['cols'])
 
 # draw plots
 canvas = QROOT.TCanvas('canvas', '', 800, 500)
@@ -82,15 +105,15 @@ if doPrint:
     plotfile = 'plots/timelt1ps_profile_%s.pdf' % 'pT_IPchi2_PIDK' # sanitise_str_src('_'.join(variables))
     canvas.Print(plotfile + '[')
 
-stats = {}
 for var in variables:
+    stats = {}
     for midx, mode in enumerate(modes):
         drawopts = 'e1'
         if midx > 0:
             drawopts += ' sames'
         hprofiles[var][mode].Draw(drawopts)
         axes[var][mode].Draw()
-        legend.AddEntry(hprofiles[var][mode], modes[mode], 'pel')
+        legend.AddEntry(hprofiles[var][mode], modes[mode]['name'], 'pel')
         stats[mode] = hprofiles[var][mode].FindObject('stats')
         if midx > 0:
             stats[mode].SetY1NDC(y1)
