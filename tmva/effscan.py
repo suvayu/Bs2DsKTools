21e00337ca3a45893330b2c84d45c55292ad7f26
Suvayu Ali <Suvayu.Ali@cern.ch>
2014-09-12 10:09:33 +0200

    effscan.py: cleanup efficiency plotting script

--- tmva/effscan.py
+++ tmva/effscan.py
@@ -6,6 +6,7 @@ from utils import _import_args
 optparser = argparse.ArgumentParser(description=__doc__)
 optparser.add_argument('filename', nargs='?', default = None, help='ROOT file')
 optparser.add_argument('-t', dest='tree', default='TestTree', help='Tree name')
+optparser.add_argument('-p', dest='doprint', action='store_true', default=False, help='Print to png/pdf files')
 options = optparser.parse_args()
 locals().update(_import_args(options))
 
@@ -25,47 +26,45 @@ variables = ['time', 'terr', 'lab0_MM']
 rfile = root_open(filename, 'read')
 tree = rfile.Get(tree)
 
-tmp = Canvas(400, 400)
+tmp = Canvas(400, 400)          # temporary canvas
+hists = [[], []]                # stored histograms: sig, bkg
+for var in variables:
+    for i in (0, 1):            # sig, bkg
+        for j, cut in enumerate(mva_cuts):
+            cuts = [Cut('BDTG>{}'.format(cut)), Cut('BDTG<={}'.format(cut))]
+            allevts = Cut('classID=={}'.format(i))
+            hnumerator = tree.Draw(var, selection = cuts[i] & allevts)
+            hdenominator = tree.Draw(var, selection = allevts)
+            heff = hnumerator.Clone('heff{}_{}_{}'.format(i, var, cut))
+            heff.Reset('icesm')
+            heff.SetTitle(cuts[i].latex() + '...')
+            heff.Divide(hnumerator, hdenominator)
+            heff.Add(F1('{}'.format(j), 0, 8000)) # offset for visibility
+
+            # aesthetics
+            heff.linecolor = colours[j]
+            heff.markercolor = colours[j]
+            heff.markersize = 0.2
+            heff.SetMaximum(6)
+            heff.SetMinimum(0)
+            heff.SetStats(False)
+            hists[i].append(heff)
+
+## Plots
+nvars = len(variables)
+ncuts = len(mva_cuts)
+
+# ROOT
 canvas = Canvas(800, 400, 500, 10)
 canvas.Divide(2, 1)
-
-for var in variables:
-    for i, cut in enumerate(mva_cuts):
-        tmp.cd()
-        sig_cut = Cut('BDTG>{}'.format(cut))
-        sig_all = Cut('classID==0')
-        hsig = tree.Draw(var, selection = sig_cut & sig_all)
-        halls = tree.Draw(var, selection = sig_all)
-        heff = [hsig.Clone('{}_{}_heffs'.format(var, cut))]
-        heff[-1].Reset('icesm')
-        heff[-1].SetTitle(sig_cut.latex() + '...')
-        heff[-1].Divide(hsig, halls)
-        heff[-1].Add(F1('{}'.format(i), 0, 8000))
-
-        bkg_cut = Cut('BDTG<={}'.format(cut))
-        bkg_all = Cut('classID==1')
-        hbkg = tree.Draw(var, selection = bkg_cut & bkg_all)
-        hallb = tree.Draw(var, selection = bkg_all)
-        heff += [hbkg.Clone('{}_{}_heffb'.format(var, cut))]
-        heff[-1].Reset('icesm')
-        heff[-1].SetTitle(bkg_cut.latex() + '...')
-        heff[-1].Divide(hbkg, hallb)
-        heff[-1].Add(F1('{}'.format(i), 0, 8000))
-
-        canvas.cd()
-        for j, hist in enumerate(heff):
-            canvas.cd(j+1)
-            hist.linecolor = colours[i]
-            hist.markercolor = colours[i]
-            hist.markersize = 0.2
-            hist.SetMaximum(6)
-            hist.SetMinimum(0)
-            hist.SetStats(False)
-            if i == 0:          # first cut
-                hist.Draw('e1')
-            else:               # other cuts
-                hist.Draw('e1 same')
+for i, var in enumerate(variables):
+    for j in (0, 1):        # sig, bkg
+        canvas.cd(j+1)
+        for k, cut in enumerate(mva_cuts):
+            opts = 'e1' if k == 0 else 'e1 same'
+            hists[j][i*ncuts + k].Draw(opts)
     canvas.Modified()
     canvas.Update()
-    for typ in ['png', 'pdf']:
-        canvas.Print('{}_bkg_sig_eff.{}'.format(var, typ))
+    if doprint:
+        for typ in ['png', 'pdf']:
+            canvas.Print('{}_bkg_sig_eff.{}'.format(var, typ))
