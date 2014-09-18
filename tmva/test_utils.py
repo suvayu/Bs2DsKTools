8d67f92e8ca2da43cf4a32b160689b4266ce5554
Suvayu Ali <Suvayu.Ali@cern.ch>
2014-09-18 14:42:51 +0200

    tmva: rename yaml description file

--- tmva/test_utils.py
+++ tmva/test_utils.py
@@ -5,18 +5,21 @@ import unittest
 from fixes import ROOT
 from utils import read_yaml, make_paths, path2rootdir
 class test_utils(unittest.TestCase):
+    def setUp(self):
+        self.yamlfile = 'tmva_output_description.yaml'
+
     def test_read_yaml(self):
-        self.assertTrue(read_yaml('dsk_train_out.yaml'))
+        self.assertTrue(read_yaml(self.yamlfile))
 
     def test_make_paths(self):
-        path_dicts = make_paths(read_yaml('dsk_train_out.yaml'))
+        path_dicts = make_paths(read_yaml(self.yamlfile))
         # test 1 generic metadata field
         self.assertTrue(path_dicts[0]['title'])
         for path in path_dicts:
             self.assertTrue(path['path'])
 
     def test_path2rootdir(self):
-        path_dicts = make_paths(read_yaml('dsk_train_out.yaml'))
+        path_dicts = make_paths(read_yaml(self.yamlfile))
         for path in path_dicts:
             rfile, rdir = path2rootdir(path['path'])
             self.assertTrue(not rfile.IsZombie())
