256f8b346763a10c79623bdef38dc0d3d1150027
Suvayu Ali <Suvayu.Ali@cern.ch>
2014-09-28 11:21:41 +0200

    tmva: ROC curve

--- tmva/performance.py
+++ tmva/performance.py
@@ -116,3 +116,37 @@ if distribs:
 
     if doprint: canvas.Print('classifiers.pdf]')
     del canvas
+
+if roc:
+    # roc curve: MVA_<name>_rejBvsS
+    roc = get_hists(classifiers, rfileconf, rpath_tool, robj_t = ROOT.TH1,
+                    robj_p = _filter('_rejBvsS'))
+
+    canvas = ROOT.TCanvas('canvas', '', 800, 600)
+    if doprint: canvas.Print('roc.pdf[')
+
+    roc = dict((k, v[0]) for k, v in roc.iteritems()) # cleanup dict
+    cols = (ROOT.kAzure, ROOT.kRed, ROOT.kBlack)
+    legend = ROOT.TLegend(0.2, 0.2, 0.6, 0.4)
+    legend.SetHeader('MVA classifiers')
+    legend.SetBorderSize(0)
+    legend.SetFillStyle(0)
+    ROOT.gStyle.SetOptStat(False)
+    for i, hist in enumerate(roc.iteritems()):
+        # hist.SetFillStyle(0)
+        hist[1].SetLineColor(cols[i])
+        hist[1].GetXaxis().SetRangeUser(0.7, 1.0)
+        hist[1].GetYaxis().SetRangeUser(0.7, 1.0)
+        legend.AddEntry(hist[1], classifiers[hist[0]], 'l')
+        if i == 0:
+            hist[1].SetTitle('MVA classifier ROC curves')
+            hist[1].Draw('c')
+        else:
+            hist[1].Draw('c same')
+    legend.Draw()
+    canvas.SetGrid(1, 1)
+    canvas.Update()
+    if doprint: canvas.Print('roc.pdf')
+
+    if doprint: canvas.Print('roc.pdf]')
+    del canvas
