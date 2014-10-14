a83a62fa78b8bcd9eff95ad02e15747efe2c9d71
Suvayu Ali <Suvayu.Ali@cern.ch>
2014-10-14 14:48:20 +0200

    tmva: restructure confquery script

--- tmva/confquery.py
+++ tmva/confquery.py
@@ -6,7 +6,8 @@ from utils import _import_args
 optparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                     description=__doc__)
 optparser.add_argument('conffile', nargs='?', default='TMVA.conf', help='TMVA config file')
-optparser.add_argument('-s', dest='session', help='Session name (maybe glob)')
+optparser.add_argument('-l', dest='sglob', help='Session name glob for listing')
+optparser.add_argument('-s', dest='session', default=None, help='Session name')
 options = optparser.parse_args()
 locals().update(_import_args(options))
 
@@ -18,18 +19,18 @@ from tmvaconfig import TMVAType, TMVAconfig, ConfigFile
 conf = ConfigFile(conffile)
 n = conf.read()
 
-print '# sessions'
-# print all sessions
-if not session:
-    for s in conf.sessions():
-        print s,
-    sys.exit(0)
+res = ''
+valid_sessions = conf.sessions()
+if sglob:                       # find requested session
+    if reduce(lambda i,j: i or j, map(lambda x: x in sglob, '*?')): # glob
+        from fnmatch import fnmatchcase
+        for s in valid_sessions:
+            if fnmatchcase(s, sglob): res = ' '.join([res, s])
+    else:                           # exact
+        if sglob in valid_sessions: res = ' '.join([res, sglob])
+elif not session:               # print all sessions
+    for s in valid_sessions: res = ' '.join([res, s])
 
-# find requested session
-if reduce(lambda i,j: i or j, map(lambda x: x in session, '*?')): # glob
-    from fnmatch import fnmatchcase
-    for s in conf.sessions():
-        if fnmatchcase(s, session): print s,
-else:                           # exact
-    if session in conf.sessions():
-        print session
+if res:
+    print '# sessions\n{}'.format(res)
+    sys.exit(0)
