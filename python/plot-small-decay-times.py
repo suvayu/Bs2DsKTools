e360a5b1b27d8791fa0e0f5a6306e4e46ffac86e
Suvayu Ali <Suvayu.Ali@cern.ch>
2015-04-17 12:11:40 +0200

    acceptance: rewrite script for pT, IP χ², & η
    - Don't use rootpy
    - Use Rplot from rplot
    
    TODO: Use Tplot from rplot

--- python/plot-small-decay-times.py
+++ python/plot-small-decay-times.py
@@ -26,34 +26,14 @@ options = optparser.parse_args()
 doPrint = options.doPrint
 reverse = options.reverse
 
-# Python modules
-import os
-import sys
-from glob import glob
-
-# FIXME: Batch running fails on importing anything but gROOT
-# ROOT global variables
-from ROOT import gROOT
-if doPrint: gROOT.SetBatch(True)
-
-from ROOT import gPad, gDirectory
-
-# ROOT colours and styles
-from ROOT import kGreen, kRed, kBlack, kBlue, kAzure, kYellow, kCyan
-from ROOT import kDot, kPlus, kFullDotMedium, kFullDotSmall
+from rplot.fixes import ROOT
+ROOT.gROOT.SetBatch(doPrint)
 
 # ROOT classes
-from ROOT import TCanvas, TLegend
-
-# rootpy modules
-from rootpy.io import File
-from rootpy.tree import Tree
-from rootpy.tree import TreeChain
-from rootpy.tree import Cut
-from rootpy.plotting import Hist, Profile
+from ROOT import TFile, TCut, TH1D, TLegend
 
 # my libs
-from helpers import *
+from helpers import sanitise_str_src, sanitise_str
 
 
 ## Read from file
@@ -61,19 +41,15 @@ if reverse:
     modes = ['dspi', 'dsk']
 else:
     modes = ['dsk', 'dspi']
-
-# trees = {
-#     'dsk'  : TreeChain('DecayTree', glob('../ntuples/MC/MC11a_AfterOfflineSel/MergedTree_Bs2DsK_*BsHypo_BDTG.root')),
-#     'dspi' : TreeChain('DecayTree', glob('../ntuples/MC/MC11a_AfterOfflineSel/MergedTree_Bs2DsPi_*BsHypo_BDTG.root'))
-# }
+pmodes = {'dsk': 'DsK', 'dspi': 'Ds#pi'}
 
 files = {
-    'dsk'  : File('data/smalltree-really-new-MC-pre-PID-DsK.root'),
-    'dspi' : File('data/smalltree-really-new-MC-pre-PID-DsPi.root')
+    'dsk' : TFile.Open('data/smalltree-really-new-MC-pre-PID-DsK.root'),
+    'dspi': TFile.Open('data/smalltree-really-new-MC-pre-PID-DsPi.root')
 }
 trees = {
-    'dsk'  : files['dsk'].Get('ftree'),
-    'dspi' : files['dspi'].Get('ftree')
+    'dsk' : files['dsk'].Get('ftree'),
+    'dspi': files['dspi'].Get('ftree')
 }
 
 variables = ('hMom.Pt()', 'hMom.Eta()', 'hIPchi2', 'time')
@@ -82,16 +58,16 @@ prettyvars = ('h p_{T}', 'h #eta', 'h IP #chi^{2}', 'decay time')
 # prettyvars = ['h IP #chi^{2}']
 
 cuts = {
-    'nocuts'    : Cut(''),
-    'timelt1ps' : Cut('time<1.0'), # from small tree
-    'dsk'       : Cut('PIDK>10'),
-    'dspi'      : Cut('PIDK<0'),
-    'bdt'       : Cut('BDTG>0.5'),
-    'HLT1'      : Cut('HLT1TrackAllL0TOS > 0'),
-    'HLT22Body' : Cut('HLT2Topo2BodyTOS > 0'),
-    'HLT23Body' : Cut('HLT2Topo3BodyTOS > 0'),
-    'HLT24Body' : Cut('HLT2Topo4BodyTOS > 0')
-    # 'timelt1ps' : Cut('lab0_TAU<1.0'), # from ntuples directly
+    'nocuts'    : TCut(''),
+    'timelt2ps' : TCut('time<2.0'), # from small tree
+    'dsk'       : TCut('PIDK>10'),
+    'dspi'      : TCut('PIDK<0'),
+    'bdt'       : TCut('BDTG>0.5'),
+    'HLT1'      : TCut('HLT1TrackAllL0TOS > 0'),
+    'HLT22Body' : TCut('HLT2Topo2BodyTOS > 0'),
+    'HLT23Body' : TCut('HLT2Topo3BodyTOS > 0'),
+    'HLT24Body' : TCut('HLT2Topo4BodyTOS > 0')
+    # 'timelt2ps' : Cut('lab0_TAU<1.0'), # from ntuples directly
     # 'dsk'       : Cut('lab1_PIDK>10'),
     # 'dspi'      : Cut('lab1_PIDK<0'),
     # 'bdt'       : Cut('BDTGResponse_1>0.5'),
@@ -100,8 +76,11 @@ cuts = {
     # 'HLT23Body' : Cut('lab0_Hlt2Topo3BodyBBDTDecision_TOS > 0'),
     # 'HLT24Body' : Cut('lab0_Hlt2Topo2BodyBBDTDecision_TOS > 0')
 }
-cuts.update(dict(HLT2 = cuts['HLT22Body'] | cuts['HLT23Body'] | cuts['HLT24Body']))
-cuts.update(dict(trig = cuts['HLT1'] & cuts['HLT2']))
+
+cuts['HLT2'] = TCut('({})||({})||({})'.format(cuts['HLT22Body'],
+                                              cuts['HLT23Body'],
+                                              cuts['HLT24Body']))
+cuts['trig'] = cuts['HLT1'] + cuts['HLT2']
 
 htypes = [
     'nocuts', 'pid', 'bdt', 'trig',    # base
@@ -115,7 +94,7 @@ hprofiles = []                  # list of dicts, keys: dsk, dspi
 
 ## pT and η distributions for different cuts
 for htype in htypes:
-    for var in variables:
+    for v, var in enumerate(variables):
         if var.lower().find('pt') >= 0:
             binning = (100, 0.0, 2E4)
         elif var.lower().find('eta') >= 0:
@@ -123,83 +102,61 @@ for htype in htypes:
         elif var.lower().find('ipchi2') >= 0:
             binning = (100, 0.0, 1E3)
         elif var.lower().find('time') >= 0:
-            binning = (100, 0.0, 1.0)
+            binning = (100, 0.0, 1.5)
         else:
             print 'Unknown variable, weird things will happen.'
         hpair = {}
         max_y_n = 0             # max Y after normalisation
         for mode in modes:
             # ensure identical binning
-            hist = Hist(*binning, name='h' + mode + '_' + htype + '_'
-                        + sanitise_str_src(var), type='D')
-            cut = cuts['timelt1ps']
+            hname = 'h{}_{}_{}'.format(mode, htype, sanitise_str_src(var))
+            # print hname
+            hist = TH1D(hname, '{0} {1} {2};{2}'
+                        .format(pmodes[mode], htype, prettyvars[v]), *binning)
+            cut = cuts['timelt2ps']
             cut_tokens = htype.split('_')
             for token in cut_tokens:
                 if token.find('nocuts') >= 0:
                     pass
                 elif token.find('pid') >= 0:
-                    cut = cut & cuts[mode]
+                    cut = cut + cuts[mode]
                 elif token.find('bdt') >= 0:
-                    cut = cut & cuts['bdt']
+                    cut = cut + cuts['bdt']
                 elif token.find('trig') >= 0:
-                    cut = cut & cuts['trig']
+                    cut = cut + cuts['trig']
                 else:
                     print 'Unknown permutation of cuts, weird things will happen.'
-            trees[mode].Draw(var, cut, '', hist)
+            sel = trees[mode].Draw('{}>>{}'.format(var, hist.GetName()), cut, 'goff')
             hpair[mode] = hist
-            # FIXME: TreeChain does some copying of histograms internally
-            if isinstance(trees[mode], TreeChain):
-                hpair[mode].SetName(hist.GetName())
             # determine max Y for normalised histograms
             if max_y_n < hpair[mode].GetMaximum()/hpair[mode].Integral():
                 max_y_n = hpair[mode].GetMaximum() / hpair[mode].Integral()
-        # set max Y so that histograms fits in pad
+        # set max Y so that histograms fit in pad
         for mode in modes:
             hpair[mode].SetMaximum(1.1 * max_y_n * hpair[mode].Integral())
         histograms.append(hpair)
 
+hists = map(lambda hs: (hs['dsk'], hs['dspi']), histograms)
 
-## plot distributions
-nvars = len(variables)
-canvas = TCanvas('canvas', '', 800*nvars, 500)
-canvas.Divide(nvars,1)
-
-# open file
-plotfile = 'plots/timelt1ps_%s.pdf' % sanitise_str_src('_'.join(variables))
+from rplot.rplot import Rplot
+plotter = Rplot(2, 2, 1200, 1000)
+canvas = plotter.prep_canvas()
+plotfile = 'plots/timelt2ps_%s.pdf' % sanitise_str_src('_'.join(variables))
 if doPrint:
     canvas.Print(plotfile + '[')
 
 # create legend
-legend = TLegend(0.6, 0.5, 0.85, 0.65)
-legend.SetLineColor(0)
+legend = TLegend(0.45, 0.4, 0.88, 0.55)
+legend.SetLineColorAlpha(0, 100)
 legend.SetFillStyle(0)
 legend.SetTextSize(0.035)
+legend.SetHeader('%s ps'.format(sanitise_str(str(cuts['timelt2ps']))))
+plotter.add_legend(legend)
 
-# draw distributions side by side
-for hidx, hpair in enumerate(histograms):
-    canvas.cd(hidx % nvars + 1)
-    var = prettyvars[hidx % nvars]
-    legend.SetHeader('%s (%s ps)' % (var, sanitise_str(cuts['timelt1ps'].str)))
-    for midx, mode in enumerate(modes):
-        htitle = ' '.join(hpair[mode].GetName().split('_')[1:])
-        hpair[mode].SetTitle(htitle + ';%s' % var)
-        drawopts = 'hist'
-        if midx > 0:
-            drawopts += ' same'
-        if mode == 'dsk':
-            hpair[mode].SetLineColor(kBlue)
-            legend.AddEntry(hpair[mode], 'DsK', 'l')
-        if mode == 'dspi':
-            hpair[mode].SetLineColor(kRed)
-            legend.AddEntry(hpair[mode], 'Ds#pi', 'l')
-        hpair[mode].DrawNormalized(drawopts)
-    legend.Draw()
-    if (hidx + 1) % nvars == 0:
-        canvas.Update()
-        if doPrint:
-            canvas.Print(plotfile)
-    legend.Clear()              # necessary for legend on both pads
-
-# close file
+for i in xrange(0, len(hists), 4):
+    plotter.draw_hist(hists[i:i+4], 'e1', normalised=True)
+    if doPrint:
+        canvas.Print(plotfile)
 if doPrint:
     canvas.Print(plotfile + ']')
+    del plotter
