6caabe18dee5c74954d9b3154871b4a43c6898eb
Suvayu Ali <Suvayu.Ali@cern.ch>
2015-05-19 23:56:47 +0200

    tmva: refactor efficiency/significance scan script

--- tmva/sigscan.py
+++ tmva/sigscan.py
@@ -41,81 +41,107 @@ optparser.add_argument('-p', dest='doprint', action='store_true',
                        help='Print to png/pdf files')
 optparser.add_argument('-b', dest='batch', action='store_true',
                        help='Run in batch mode')
-optparser.add_argument('-c', dest='classifier', default='BDTB',
-                       help='Classifier to plot')
-optparser.add_argument('-w', dest='what', choices=['eff', 'sig', 'both'], default='both',
+optparser.add_argument('-c', dest='classifier', default='BDTG',
+                       choices=['BDTG', 'BDTB', 'BDTGResponse_1'],
+                       help='MVA classifier to plot')
+optparser.add_argument('-w', dest='what', default='both',
+                       choices=['eff', 'sig', 'both'],
                        help='What to plot: efficiency, significance, both')
-optparser.add_argument('-T', dest='istmva', action='store_true',
-                       help='Using TMVA output as input file')
-optparser.add_argument('-n', dest='tree', default='',
-                       help='Tree name to use')
-optparser.add_argument('-s', dest='session',
-                       help='Mandatory TMVA session config when using MC '
-                       '(significance calc not possible)')
+optparser.add_argument('--tree', default='', help='Tree name to use')
 optparser.add_argument('--conf', default='TMVA.conf',
                        help='TMVA configuration file')
+optparser.add_argument('--istmva', action='store_true',
+                       help='Using TMVA output as input file')
+optparser.add_argument('--isdata', action='store_true',
+                       help='Using data as input')
+optparser.add_argument('--ismc', action='store_true',
+                       help='Using Monte Carlo as input')
+optparser.add_argument('-s', '--session', help='Mandatory TMVA training '
+                       'session (required when using MC, otherwise '
+                       'significance calculation is not possible)')
 options = optparser.parse_args()
-locals().update(_import_args(options))
+
+import sys
+mandatory = [getattr(options, i) for i in vars(options) if i.startswith('is')]
+if True not in mandatory:
+    # \b or \x08 = backspace
+    sys.exit('one of {} mandatory'.format(
+        reduce(lambda i, j: '{} --{}'.format(i, j), mandatory, '\b')))
+
+if options.ismc and not options.session:
+    sys.exit('missing --session: {}'.format(
+        optparser._option_string_actions['--session'].help))
 
 # what to calculate
-eff = what in ['eff', 'both']   # efficiency
-sig = what in ['sig', 'both']   # significance
-mc = bool(session)
+eff = options.what in ['eff', 'both']   # efficiency
+sig = options.what in ['sig', 'both']   # significance
+if options.ismc and sig:        # consistency check
+    print 'Cannot calculate significance with MC, will calc only eff'
+    sig = False
+
+# for convenience
+istmva = options.istmva
+ismc = options.ismc
+isdata = options.isdata
+classifier = options.classifier
+
+from utils import session_from_path
+prefix = 'plots/{}'
+if options.session:
+    prefix = prefix.format(options.session)
+else:
+    prefix = prefix.format(session_from_path(options.rfile))
+
+
+from fixes import ROOT
+ROOT.gROOT.SetBatch(options.batch)
+
 cltitle = {
     'BDTB': 'BDT cuts (w/ bagging)',
     'BDTG': 'BDT cuts (w/ gradient boost)',
     'BDTGResponse_1': 'Old BDT cuts (w/ gradient boost)'
 }
 
-# option consistency checks
-errmsg = 'Incompatible options: {}.'
-import sys
-if session:
-    if what == 'both':
-        print 'Cannot calculate significance with MC, will calc only eff.'
-    elif what == 'sig':
-        sys.exit(errmsg.format('session and significance'))
-    else:
-        eff, sig = True, False
-
-from fixes import ROOT
-ROOT.gROOT.SetBatch(batch)
-
-tree = tree if tree else 'TestTree' if istmva else 'DecayTree'
-if istmva:             # TMVA output
-    rfile = ROOT.TFile.Open(rfile, 'read')
-    tree = rfile.Get(tree)
+# read tree
+if options.tree:
+    tree = options.tree
 else:
-    rfile = ROOT.TFile.Open(rfile, 'read')
-    tree = rfile.Get(tree)
+    tree = 'TestTree' if options.istmva else 'DecayTree'
+rfile = ROOT.TFile.Open(options.rfile, 'read')
+tree = rfile.Get(tree)
 
-## sig, bkg, and other cuts
+# sig, bkg, and other cuts
 if istmva:
     sig, bkg = 'classID=={}'.format(0), 'classID=={}'.format(1) # TMVA
     nbkg = float(tree.GetEntries(bkg))
     nsig = float(tree.GetEntries(sig))
-elif mc:
+elif ismc:
     from tmvaconfig import ConfigFile
-    conf = ConfigFile(conf)
+    conf = ConfigFile(options.conf)
     if conf.read() > 0:
-        session = conf.get_session_config(session)
+        session = conf.get_session_config(options.session)
     else:
         sys.exit('No sesions found!')
     sig = session.cut_sig
     nsig = float(tree.GetEntries(sig))
-else:
-    ispion = 'lab1_PIDK<-5'                                       # data
-    region1 = '5300<lab0_MM && lab0_MM<5445 && {}'.format(ispion) # ~ s + b(w/ π)
-    region2 = '5445<lab0_MM && lab0_MM<5800'                      # ~ b
-    region3 = '5445<lab0_MM && lab0_MM<5800 && {}'.format(ispion) # ~ b(w/ π)
+else:                           # isdata
+    ispion = 'lab1_PIDK<-5'
+    # ~ s + b(w/ π) (FIXME: update range)
+    region1 = '5300<lab0_MM && lab0_MM<5445 && {}'.format(ispion)
+    # ~ b
+    region2 = '5445<lab0_MM && lab0_MM<5800'
+    # ~ b(w/ π)
+    region3 = '5445<lab0_MM && lab0_MM<5800 && {}'.format(ispion)
     nregion1 = float(tree.GetEntries(region1))
     nregion2 = float(tree.GetEntries(region2))
     nregion3 = float(tree.GetEntries(region3))
+
 
 # signal and background estimates from data
 def get_bkg(nevts2, lo=5300, mid=5445, up=5800):
     """n2 = (5800-5445)/(5800-5300) * b"""
     return nevts2 * float(up-lo)/float(up-mid)
+
 
 def get_sig(nevts1, nevts3, lo=5300, mid=5445, up=5800):
     """n1 = (5445-5300)/(5800-5300) * b(w/ π) + s
@@ -127,18 +153,18 @@ def get_sig(nevts1, nevts3, lo=5300, mid=5445, up=5800):
 nevts_passed = []
 if istmva:
     nevts_passed += [
-        lambda tree, cut: tree.GetEntries('{}&&{}'.format(sig, cut.greaterequal)),
-        lambda tree, cut: tree.GetEntries('{}&&{}'.format(bkg, cut.greaterequal))
+        lambda tree, cut: tree.GetEntries('{}&&{}'.format(sig, cut.ge)),
+        lambda tree, cut: tree.GetEntries('{}&&{}'.format(bkg, cut.ge))
     ]
-elif mc:
+elif ismc:
     nevts_passed += [
-        lambda tree, cut: tree.GetEntries('{}&&{}'.format(sig, cut.greaterequal))
+        lambda tree, cut: tree.GetEntries('{}&&{}'.format(sig, cut.ge))
     ]
-else:
+else:                           # isdata
     nevts_passed += [
-        lambda tree, cut: tree.GetEntries('{}&&{}'.format(region1, cut.greaterequal)),
-        lambda tree, cut: tree.GetEntries('{}&&{}'.format(region2, cut.greaterequal)),
-        lambda tree, cut: tree.GetEntries('{}&&{}'.format(region3, cut.greaterequal))
+        lambda tree, cut: tree.GetEntries('{}&&{}'.format(region1, cut.ge)),
+        lambda tree, cut: tree.GetEntries('{}&&{}'.format(region2, cut.ge)),
+        lambda tree, cut: tree.GetEntries('{}&&{}'.format(region3, cut.ge))
     ]
 
 from numpy import linspace, array
@@ -148,26 +174,27 @@ else:
     clrange = (-1.0, 1.0)
 cuts = linspace(clrange[0], clrange[1], 101)
 
-## significance, signal selection and background rejection efficiency
+# significance, signal selection and background rejection efficiency
 from utils import scan_range
 res = array(scan_range(nevts_passed, cuts, classifier, tree))
 
 from math import sqrt
 if istmva:
     if eff:
-        effs = [0, 1] + res / [nsig, -nbkg] # sel_s/tot_s, 1-sel_b/tot_s
-        eff_s, eff_b = effs[:,0], effs[:,1]
+        # sel_s/tot_s, 1-sel_b/tot_s
+        effs = array([0, 1]) + res / array([nsig, -nbkg])
+        eff_s, eff_b = effs[:, 0], effs[:, 1]
 
     if sig:
-        res /= [16.0, 1.0]                           # Dsπ/DsK ~ 16
-        sigs = res[:,0] / map(sqrt, res.sum(axis=1)) # σ = s/sqrt(s+b)
-elif mc:
+        res /= array([16.0, 1.0])                      # Dsπ/DsK ~ 16
+        sigs = res[:, 0] / map(sqrt, res.sum(axis=1))  # σ = s/sqrt(s+b)
+elif ismc:
     effs = res/nsig
-else:
+else:                        # isdata
     # approximate signal and backround events
-    res1 = res[:,0]
-    res2 = res[:,1]
-    res3 = res[:,2]
+    res1 = res[:, 0]
+    res2 = res[:, 1]
+    res3 = res[:, 2]
     evt_s = array(map(get_sig, res1, res3))
     evt_b = array(map(get_bkg, res2))
 
@@ -178,14 +205,15 @@ else:
     if sig:
         evt_s /= 16.0
         sigs = evt_s / map(sqrt, evt_s + evt_b)
-        # agns = eff_s * evt_s / (evt_s + evt_b) # FIXME: requires eff_s
+        # agns = eff_s * evt_s / (evt_s + evt_b)  # FIXME: requires eff_s
 
 from numpy import isnan                      # clean NaNs
 sigs = map(lambda i: 0 if isnan(i) else i, sigs)
 # agns = map(lambda i: 0 if isnan(i) else i, agns)
 
-## plot
-from ROOT import TH1D, TProfile
+
+# fill histograms
+from ROOT import TProfile
 nbins = 40
 bins = linspace(clrange[0], clrange[1], nbins + 1)
 hist_s = TProfile('hist_s', 'Signal selection efficiency', nbins, bins)
@@ -199,16 +227,20 @@ map(th1fill(hist_b, 2), cuts, eff_b)
 map(th1fill(hsigma, 2), cuts, sigs)
 # map(th1fill(hagns, 2), cuts, agns)
 
+# plot
 import matplotlib as mpl
 mpl.rc('font', family='Liberation Sans') # choose font
 mpl.rc('mathtext', default='regular')    # use default font for math
 
-if batch: mpl.use('pdf')    # plotting w/o X11
-else: import matplotlib.pyplot as plt
+if options.batch:
+    mpl.use('pdf')    # plotting w/o X11
+else:
+    import matplotlib.pyplot as plt
 
 # PDF backend
 from matplotlib.backends.backend_pdf import PdfPages, FigureCanvasPdf
-if doprint: pp = PdfPages('significance_{}.pdf'.format(classifier))
+if options.doprint:
+    pp = PdfPages('{}_significance_{}.pdf'.format(prefix, classifier))
 
 # use the API
 from matplotlib.figure import Figure
@@ -238,29 +270,34 @@ x, y, yerr = th12errorbar(hsigma, yerr=True)
 axes2.errorbar(x, y, yerr=yerr, xerr=None, fmt=None, ecolor='black',
                label=hist.GetTitle())
 
-## annotations
+# annotations
 if classifier == 'BDTGResponse_1':
     axes.plot([0.3, 0.3], [0, 1], color='black', linewidth=2)
     axes.text(0.35, 0.3, 'Old working pt ($\geq 0.3$)',
               bbox=dict(boxstyle='larrow', fc='white', ec='black'))
     oeff_s = hist_s.GetBinContent(hist_s.FindBin(0.3))
-    axes.annotate('(0.3,{:.3f})'.format(oeff_s), xy=(0.3,oeff_s), xytext=(-0.1,0.75),
-                  arrowprops=dict(facecolor='black', shrink=0.1, width=2,
-                                  headwidth=5))
-if classifier == 'BDTB':
+    axes.annotate('(0.3,{:.3f})'.format(oeff_s), xy=(0.3, oeff_s),
+                  xytext=(-0.1, 0.75), arrowprops=dict(
+                      facecolor='black', shrink=0.1, width=2, headwidth=5))
+elif classifier == 'BDTB':
     diff, idx = 1., None
     for i, eff in enumerate(eff_s):
         diff_t = abs(eff-0.842)     # eff_s @ old BDTG >= 0.3
-        if diff > diff_t: diff, idx = diff_t, i
+        if diff > diff_t:
+            diff, idx = diff_t, i
     axes.plot(clrange, [0.842, 0.842], color='black', linewidth=2)
     axes.annotate('Working pt @ old eff {}'.format((cuts[idx], 0.842)),
                   xy=(cuts[idx], 0.842), xytext=(0.0, 1.1), ha='right',
                   arrowprops=dict(facecolor='black', shrink=0.05,
                                   width=2, headwidth=5))
+elif classifier == 'BDTG':
+    print 'FIXME: implement me: annotations for BDTG'
+else:
+    print 'Shouldn\'t get here!'
 
-if doprint:
+if options.doprint:
     pp.savefig(fig)
     pp.close()
 
-if not batch:
+if not options.batch:
     plt.show()
